1cc1812361015852de8b501efbc0273d218cd156
Erkan Ergen <erkanergen35@gmail.com>
2021-02-07 17:48:29 +0300

    Add project files.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+
+        public int MusteriId { get; set; }
+        public string MusteriNo { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriId = 1;
+            musteri1.MusteriNo = "1234567890";
+            musteri1.Adi = "Hansel";
+            musteri1.Soyadi = "German";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriId = 2;
+            musteri2.MusteriNo = "1456787654";
+            musteri2.Adi = "Gratel";
+            musteri2.Soyadi = "German";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.MusteriId = 3;
+            musteri3.MusteriNo = "123367890";
+            musteri3.Adi = "Erkan";
+            musteri3.Soyadi = "Ergen";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriEkle(musteri3);
+
+            Musteri[] musteri = new Musteri[] { musteri1, musteri2, musteri3 };
+            musteriManager.MusteriListele(musteriler);
+            musteriManager.MusteriSil(musteri1);
+        }
+    }
+}
